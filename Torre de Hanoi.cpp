899991492d08415d888342ca2c3938008982a4f5
GabrielRolon <67517159+GabrielRolon@users.noreply.github.com>
2020-09-20 14:03:12 -0300

    Update de dialogo

--- Torre de Hanoi.cpp
+++ Torre de Hanoi.cpp
@@ -10,6 +10,9 @@ int main()
     system("color B0");
     cout<<"Hi, i'm BMO, Nice to meet you"<<endl;
     system("pause");
+    system("cls");
+    cout<<"I'm a little living boy"<<endl;
+    system("pause");
 
     return 0;
 }
